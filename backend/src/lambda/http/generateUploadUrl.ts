e749b1199fe482c5a7574174f840b8b212e486dd
nembo005 <nembotchedjou35@gmail.com>
2023-06-02 11:37:06 +0100

    updated agendaID

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -4,15 +4,15 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { createAttachmentPresignedUrl } from '../../businessLogic/todos'
+import { createAttachmentPresignedUrl } from '../../businessLogic/agenda'
 import { getUserId } from '../utils'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const todoId = event.pathParameters.todoId
+    const agendaId = event.pathParameters.agendaId
     // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
     const id = getUserId(event)
-    const presignedUrl = await createAttachmentPresignedUrl(id, todoId)
+    const presignedUrl = await createAttachmentPresignedUrl(id, agendaId)
 
     return { 
       statusCode: 200, 
